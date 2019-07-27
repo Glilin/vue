57bbc83e60ce8739a83b1c78b28766c77500a684
李林 <1576378853@qq.com>
2019-07-27 16:19:11 +0800

    vue框架

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
-   现在项目开始了
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style lang="less">
